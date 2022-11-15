9d5041360bcc5f5c0cd2900284d6d4d1a4a6cbe9
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-11-15 18:23:44 +0530

    Feat: React-Facts ✅

---
+++ react-facts/src/components/MainContent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function MainContent() {
+	return (
+		<main className="main">
+			<h2 className="main--title">
+				Fun facts about React
+			</h2>
+			<ul className="main--facts">
+				<li>
+					Was first released in 2013 Was
+					originally
+				</li>
+				<li>created by Jordan Walke</li>
+				<li>Has well over 100K stars on GitHub</li>
+				<li>Is maintained by Facebook</li>
+				<li>
+					Powers thousands of enterprise apps,
+					including mobile apps
+				</li>
+			</ul>
+		</main>
+	);
+}
+
+export default MainContent;
