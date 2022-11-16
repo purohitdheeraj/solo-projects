9e28b9bd54279e9b23c5a67b1c256837ac3a5ce9
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-11-16 17:53:20 +0530

    feat: multiple cards (variations) ✅

--- business-card/src/App.jsx
+++ business-card/src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
-
-import Footer from "./components/CardFooter";
-import Main from "./components/CardBody";
-import CardHeader from "./components/CardHeader";
+import Card from "./Card";
 
 function App() {
 	return (
 		// for className we are using BEM means parent__child--type
-		
-		<div className="card--wrapper">
-			<div className="card">
-				<CardHeader />
-				<Main />
-				<Footer />
-			</div>
+		<div className="container">
+			<Card />
+			<Card />
+			<Card />
+			<Card />
 		</div>
 	);
 }
