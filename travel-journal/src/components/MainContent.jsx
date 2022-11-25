2bc0da5159349e2c2ee3115cb1ab77c30e101a3b
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2022-11-25 08:45:17 +0530

    feat: props added ✅

--- travel-journal/src/components/MainContent.jsx
+++ travel-journal/src/components/MainContent.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Tile from "./Tile";
+import travelData from "../data";
 
 function MainContent() {
-	return (
-		<main>
-			<Tile />
-			<Tile />
-			<Tile />
-			<Tile />
-		</main>
-	);
+	const tiles = travelData.map((tile) => (
+		<Tile {...tile} />
+	));
+	return <main>{tiles}</main>;
 }
 
 export default MainContent;
--- travel-journal/src/components/Tile.jsx
+++ travel-journal/src/components/Tile.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import locateIcon from "../assets/locate-icon.svg";
 
-function Tile() {
+function Tile(props) {
+	console.log(props);
+
 	return (
 		<div className="tile">
 			<img
-				src="https://images.unsplash.com/photo-1618278942403-e973260cc425?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+				src={props.imgUrl}
 				className="tile--image"
-				alt="Mount Fuji"
+				alt={props.title}
 			/>
 			<div className="tile--info">
 				<div className="location">
@@ -17,25 +19,21 @@ function Tile() {
 						alt="locate-icon"
 					/>
 					<span className="location--name">
-						JAPAN
+						{props.location}
 					</span>
 
 					<a
 						className="location--link"
-						href="https://goo.gl/maps/yFfrGTDAKpcihWkL6"
+						href={props.googleMapsUrl}
 					>
 						View on Google Maps
 					</a>
 				</div>
-				<h2>Mount Fuji</h2>
-				<h4>12 Jan, 2021 - 24 Jan, 2021</h4>
-				<p>
-					Mount Fuji is the tallest mountain in
-					Japan, standing at 3,776 meters (12,380
-					feet). Mount Fuji is the single most
-					popular tourist site in Japan, for both
-					Japanese and foreign tourists.
-				</p>
+				<h2>{props.title}</h2>
+				<h4>
+					{props.startDate} - {props.endDate}
+				</h4>
+				<p>{props.description}</p>
 			</div>
 		</div>
 	);
